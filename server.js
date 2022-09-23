2e47f6b2c4ab4be19537532063a63fae5e4b412c
rekredhead <rekredhead@gmail.com>
2022-09-23 09:49:07 +0530

    Enabled CORS in the server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const Router = require('./router');
+const cors = require('cors');
 
 // The API that handles the requests and responses
 const app = express();
 
 app.use(express.json());
+app.use(cors()); // Enable cors to allow site to run on GitHub pages
 app.use(Router);
 
 // Run the server at localhost:3000 if no port exists
