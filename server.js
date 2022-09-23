3b965fd6de260687e12130cd386299c6b0f50382
rekredhead <rekredhead@gmail.com>
2022-09-23 09:19:35 +0530

    Refactored Code

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const Router = require('./router');
 
+// The API that handles the requests and responses
 const app = express();
 
 app.use(express.json());
 app.use(Router);
 
+// Run the server at localhost:3000 if no port exists
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server Listening at Port ${port}`);
